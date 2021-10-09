74c310638431aca88e442edc5cc996891ee9d89d
草鞋没号 <308487730@qq.com>
2021-10-09 16:47:19 +0800

    chore: UPDATE

--- script/build-main.ts
+++ script/build-main.ts
@@ -54,4 +54,3 @@ const spinner = ora(`${TAG} Electron main build...`)
     }
   }
 })();
-
--- vite.config.ts
+++ vite.config.ts
@@ -4,24 +4,11 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import styleImport from 'vite-plugin-style-import'
 
 export default defineConfig({
   plugins: [
     vue(),
-    vueJsx({/* options are passed on to @vue/babel-plugin-jsx */ }),
-    // 按需加载antd style文件
-    styleImport({
-      libs: [
-        {
-          libraryName: "ant-design-vue",
-          esModule: true,
-          resolveStyle: (name) => {
-            return `ant-design-vue/es/${name}/style/index.js`;
-          },
-        },
-      ],
-    }),
+    vueJsx(),
   ],
   root: path.join(__dirname, 'src/render'),
   base: './',
@@ -48,17 +35,5 @@ export default defineConfig({
   esbuild: {
     jsxFactory: 'h',
     jsxFragment: 'Fragment',
-    // jsxInject: `import { h } from 'vue'`, 通过 @vitejs/plugin-vue-jsx 解决
-  },
-  css: {
-    preprocessorOptions: {
-      less: {
-        // 通过modifyVars定义、覆盖全局less变量可设置ant-design-vue主题相关样式
-        // modifyVars: {
-        //   "primary-color": "#343f4c",
-        // },
-        javascriptEnabled: true,
-      },
-    },
   },
 })
